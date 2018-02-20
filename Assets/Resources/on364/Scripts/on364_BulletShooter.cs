84d930d7b647cc9eb05a93c680fc4da941bac03c
Unknown <heianhourai@gmail.com>
2018-02-20 00:39:59 -0500

    Added a prefab test room and ball spawners. They would be bullet shooters if I coudl FIGURE OUT WHAT WAS GOING ON.

---
+++ Assets/Resources/on364/Scripts/on364_BulletShooter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class on364_BulletShooter : Wall
+{
+    public GameObject bullet;
+    public float timeSinceLastShot;
+    public Vector3 shotDir;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    protected override void updateSpriteSorting()
+    {
+        // DON'T. Keep us sorted on the floor!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeSinceLastShot > .25f)
+        {
+            GameObject shot = Instantiate(bullet, transform.position+shotDir.normalized*1.5f, Quaternion.identity);
+            shot.transform.eulerAngles = shotDir;
+            shotDir += new Vector3(0, 0, 15f);
+            timeSinceLastShot = 0;
+            Debug.Log(shot.transform.rotation.eulerAngles);
+        }
+        timeSinceLastShot += Time.deltaTime;
+    }
+
+}
---
+++ Assets/Resources/on364/Scripts/on364_shot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class on364_shot : Tile {
+
+
+    public float deathTimer = 2f;
+
+    public float damageThreshold = 4;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (deathTimer > 0f)
+        {
+            deathTimer = deathTimer - Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    protected override void updateSpriteSorting()
+    {
+        // DON'T. Keep us sorted on the floor!
+    }
+
+    public virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Tile>() != null)
+        {
+            float impact = collisionImpactLevel(collision);
+            if (impact < damageThreshold)
+            {
+                return;
+            }
+            Tile otherTile = collision.gameObject.GetComponent<Tile>();
+            otherTile.takeDamage(this, 1);
+        }
+    }
+}
